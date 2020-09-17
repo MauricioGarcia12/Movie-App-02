3f6932c7dc6f1055e204f2a6134583f5a1e3dd2b
maumainsaw <maumainsaw@gmail.com>
2020-09-16 22:51:22 -0500

    Creando un Custom Hook para la llamada de la API con caracteristicas de la Pelicula

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -5,10 +5,13 @@ import Actor from './elements/Actor'
 import Grid from './elements/Grid'
 import MovieInfoBar from './elements/MovieInfoBar'
 import Spinner from './elements/Spinner'
-
+import useMovieFetch from './hooks/useMovieFetch';
 
 
 const Movie = ({movieId}) => {
+    const [movie,loading,error]= useMovieFetch(movieId)
+    console.log(movie)
+
     return ( 
         <Fragment>
             <Navigation/>
